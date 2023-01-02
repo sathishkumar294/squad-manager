e0630f3cbd23ffbc050f99e5e31ba6b6f3ae17fc
sathishkumar294 <sathishkumar.aero51@gmail.com>
2023-01-03 00:38:59 +0530

    show fill colr in tracker

--- src/shared/Slots.jsx
+++ src/shared/Slots.jsx
@@ -1,12 +1,19 @@
 import { Col, Progress, Row } from "antd";
 import React from "react";
+import { blue, red, green } from "@ant-design/colors";
 
 const Slots = ({ max, value, min }) => {
   const percent = Math.ceil((value * 100) / max);
   let status = "";
+  let color = blue;
   if (value < min) status = "exception";
   if (value > min && value < max) status = "active";
-  if (value > max) status = "exception";
+  if (value === max) color = green;
+  if (value > max) {
+    status = "exception";
+    color = red;
+  }
+  const colorArray = Array(value).fill(color[4]);
   return (
     <Row wrap={false}>
       <Col>
@@ -14,6 +21,7 @@ const Slots = ({ max, value, min }) => {
           {...{ status, percent }}
           steps={max}
           showInfo={false}
+          strokeColor={colorArray}
         ></Progress>
       </Col>
       <Col>
