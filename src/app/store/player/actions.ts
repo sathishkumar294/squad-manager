6d3182a528e33f63a14a0f4d40ad07cec50bcf1d
sathishkumar294 <sathishkumar.aero51@gmail.com>
2023-01-07 01:02:53 +0530

    add redux thunk

--- src/app/store/player/actions.ts
+++ src/app/store/player/actions.ts
@@ -1,8 +1,3 @@
-import { Dispatch } from "@reduxjs/toolkit";
 import { playerSlice } from "./slice";
 
-const { loadPlayersForTeam } = playerSlice.actions;
-
-export const loadPlayersForTeamAsync =
-  (country: string, players: Player[]) => (dispatch: Dispatch<any>) =>
-    dispatch(loadPlayersForTeam({ country, players }));
+export const { loadPlayersForTeam } = playerSlice.actions;
