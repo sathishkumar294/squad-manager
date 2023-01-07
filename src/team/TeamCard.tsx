73cfc6248c59c9a6547f739cd8bcda95466c2867
sathishkumar294 <sathishkumar.aero51@gmail.com>
2023-01-08 00:21:23 +0530

    progress colors

--- src/team/TeamCard.tsx
+++ src/team/TeamCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Progress } from "antd";
+import { Card, Progress, Space } from "antd";
 import React from "react";
 import { MAX_PLAYERS_PER_TEAM } from "../constants/config";
 import TeamCover from "./TeamCover";
@@ -6,21 +6,39 @@ import TeamCover from "./TeamCover";
 const TeamCard: React.FC<{ team: Team; onClick?: () => void }> = ({
   team,
   onClick,
-}) => (
-  <Card
-    title={team.name}
-    cover={<TeamCover team={team}></TeamCover>}
-    onClick={onClick || (() => {})}
-    hoverable={true}
-    style={{ minWidth: "200px", ...(!!onClick ? { cursor: "pointer" } : {}) }}
-  >
-    <Progress
-      percent={Math.ceil((100 * team.players.length) / MAX_PLAYERS_PER_TEAM)}
-      steps={MAX_PLAYERS_PER_TEAM}
-      size="default"
-      strokeColor={"#8E44AD"}
-    ></Progress>
-  </Card>
-);
+}) => {
+  let strokeColor = "#8E44AD";
+  switch (Math.sign(team.players.length - MAX_PLAYERS_PER_TEAM)) {
+    case 0:
+      strokeColor = "#28B463";
+      break;
+    case 1:
+      strokeColor = "#E74C3C";
+      break;
+  }
+  return (
+    <Card
+      title={team.name}
+      cover={<TeamCover team={team}></TeamCover>}
+      onClick={onClick || (() => {})}
+      hoverable={true}
+      style={{ minWidth: "200px", ...(!!onClick ? { cursor: "pointer" } : {}) }}
+    >
+      <Space direction="vertical">
+        <Progress
+          percent={Math.ceil(
+            (100 * team.players.length) / MAX_PLAYERS_PER_TEAM
+          )}
+          steps={MAX_PLAYERS_PER_TEAM}
+          size="default"
+          strokeColor={strokeColor}
+        ></Progress>
+        <span>
+          {team.players.length}/{MAX_PLAYERS_PER_TEAM}
+        </span>
+      </Space>
+    </Card>
+  );
+};
 
 export default TeamCard;
